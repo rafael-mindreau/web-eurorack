5a3f4a0b3cc82475c56822e9af3a9df8132cea8b
Rafael Mindreau <rafael.mindreau@gmail.com>
2022-06-30 15:20:28 +0200

    Sound test for good measure

--- src/components/modules/VCO-1.js
+++ src/components/modules/VCO-1.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PJ301 from '../devices/PJ301';
 import { EurorackContext } from '../managers/Eurorack';
 import { JACK_TYPES } from '../../utils/jack';
@@ -7,7 +7,7 @@ import { HORIZONTAL_PITCH_TO_PIXEL_RATIO } from '../../constants/units';
 import Panel from '../Panel';
 import DaviesKnob from '../devices/DaviesKnob';
 
-import './VCO-1.scss';
+import './generic.scss';
 
 const PANEL_COLOR = '#dbdbdb';
 const HP = 10;
@@ -16,7 +16,7 @@ const U = 3;
 const jacks = [
   {
     id: 0,
-    type: JACK_TYPES.OUTPUT,
+    type: JACK_TYPES.INPUT,
     x: 50,
     y: 430,
   },
@@ -60,15 +60,45 @@ export default ({
     endCable,
   } = useContext(EurorackContext);
   const offsetInPixels = offset * HORIZONTAL_PITCH_TO_PIXEL_RATIO;
+  const [context, setContext] = useState(null);
+  const [toneNode, setToneNode] = useState(null);
 
-  const changerFrequency = () => {
-
+  const changeFrequency = (value) => {
+    toneNode.parameters.get('frequency').value = value;
   };
 
+  const play = async () => {
+    if (!context && !toneNode) {
+      console.log('%cPLAY', 'background-color: #4eb8d6; padding: 5px; border-radius: 3px; font-weight: bold; color: white');
+      const ctx = new AudioContext();
+      await ctx.audioWorklet.addModule('worklet/processor.js');
+      setContext(ctx);
+
+      const toneNode = new AudioWorkletNode(ctx, 'tone-processor');
+      console.log('%cDEBUG', 'background-color: #1962dd; padding: 5px; border-radius: 3px; font-weight: bold; color: white', toneNode.parameters.get('sampleRate'));
+      toneNode.parameters.get('sampleRate').value = ctx.sampleRate;
+      setToneNode(toneNode);
+
+      toneNode.connect(ctx.destination);
+    }
+  }
+
   return (
-    <g transform={`translate(${offsetInPixels} 0)`} className="eurorack-module-panel VCO-1">
+    <g onClick={play} transform={`translate(${offsetInPixels} 0)`} className="eurorack-module-panel VCO-1">
       <Panel fill={PANEL_COLOR} hp={HP} moduleHeight={U} />
-      <DaviesKnob scale={0.7} x={40} y={40} onChange={changerFrequency} offset={offsetInPixels} />
+      <DaviesKnob
+        scale={0.7}
+        x={40}
+        y={40}
+        onClick={play}
+        onChange={changeFrequency}
+        offset={offsetInPixels}
+        minValue={0}
+        maxValue={100}
+        initValue={50}
+        minDegrees={0}
+        maxDegrees={359}
+      />
       <text textAnchor="middle" className="panel-header-text" x="100" y="30">VCO-1</text>
       {jacks.map((jack) => {
         const { id, x, y } = jack;
